7a5bf7f8350ae8886575b2354dfa27aa260c440e
Christoph Blessing <33834216+cblessing24@users.noreply.github.com>
2022-12-06 17:41:59 +0100

    Add solution to tuning trouble puzzle

---
+++ day6_tuning-trouble.py
@@ -0,0 +1,28 @@
+with open("day6.txt", "r") as file:
+    puzzle_input = file.readline()
+
+
+class Buffer:
+    def __init__(self, length):
+        self.length = length
+        self._buffer = []
+
+    def append(self, value):
+        self._buffer.append(value)
+        while len(self._buffer) > self.length:
+            del self._buffer[0]
+
+    def __iter__(self):
+        return iter(self._buffer)
+
+
+if __name__ == "__main__":
+    length = 14
+    buffer = Buffer(length=length)
+    for i, char in enumerate(puzzle_input):
+        buffer.append(char)
+        if len(set(buffer)) == length:
+            print(f"Start-of-packet marker '{''.join(buffer)}' received after {i + 1} characters")
+            break
+
+
